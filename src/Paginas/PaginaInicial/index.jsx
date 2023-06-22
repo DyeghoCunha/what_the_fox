1411d4264f4d2ae30362d83854a8a1c1b7daf309
Dyegho <dyeghocunha@gmail.com>
2023-06-22 15:44:56 -0300

    (feat) CardPremiunAzul OK

--- src/Paginas/PaginaInicial/index.jsx
+++ src/Paginas/PaginaInicial/index.jsx
@@ -16,7 +16,7 @@ export default function PaginaInicial() {
       <div className={styles.container}>
         <Categorias />
         <Destaques/>
-        <Deck tipo={"CardAzul"} tribo={goblin} />
+        <Deck tipo={"CardPremiunAzul"} tribo={goblin} />
       </div>
     </>
   )
